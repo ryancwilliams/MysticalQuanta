5186ae7cd0788afbb09b443cd3e6872c8d9f8fb8
ryancwilliams <rcw4rcw@gmail.com>
2023-07-01 19:58:51 -0400

    #2 Added comment to interface.
    The comment indicates that the returnned value should be final.

--- src/main/java/com/github/ryancwilliams/mysticalquanta/quanta/graph/RecipeAssetInfo.java
+++ src/main/java/com/github/ryancwilliams/mysticalquanta/quanta/graph/RecipeAssetInfo.java
@@ -9,12 +9,18 @@ public interface RecipeAssetInfo {
 
 	/**
 	 * Gets the {@link QuantaAssetInfo} that is in this {@link RecipeAssetInfo}.
+	 * <p>
+	 * The value of this field should be final.
+	 *
 	 * @return The Quanta Asset that is in this Recipe Asset.
 	 */
 	public QuantaAssetInfo getQuantaAsset();
 
 	/**
 	 * The number of {@link QuantaAssetInfo} that is in this {@link RecipeAssetInfo}.
+	 * <p>
+	 * The value of this field should be final.
+	 *
 	 * @return The number of Quanta Assets that is in this Recipe Asset.
 	 */
 	public int getQuantity();
